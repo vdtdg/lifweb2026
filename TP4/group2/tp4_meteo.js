d1420407333f494e9758f03275dd3af90acc81cb
Valerian de Thezan de Gaussan <valerian@dtdg.fr>
2026-03-10 13:03:28 +0100

    TP4 groupe 2

--- TP4/group2/tp4_meteo.js
+++ TP4/group2/tp4_meteo.js
@@ -37,3 +37,70 @@ fetch(osloUrl)
   .catch((error) => {
     console.error("Erreur meteo Oslo:", error);
   });
+
+// Exercice 2
+
+async function getGeoCoordinates(address) {
+  const apiResponse = await fetch(`https://data.geopf.fr/geocodage/search?&q=${address}`)
+  const jsonResponse = await apiResponse.json()
+  const coord = jsonResponse.features[0].geometry.coordinates
+  return { lon: coord[0], lat: coord[1] }
+}
+
+async function getMeteoFromCoordinates(coord) {
+  const meteoUrl = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${coord.lat}&lon=${coord.lon}`;
+  const meteoResponse = await fetch(meteoUrl)
+  const meteoJson = await meteoResponse.json()
+  const firstTimeserie = meteoJson?.properties?.timeseries?.[0];
+
+  return {
+    temperature: firstTimeserie?.data?.instant?.details?.air_temperature,
+    symbol: firstTimeserie?.data?.next_1_hours?.summary?.symbol_code ||
+      firstTimeserie?.data?.next_6_hours?.summary?.symbol_code ||
+      firstTimeserie?.data?.next_12_hours?.summary?.symbol_code
+  }
+}
+
+(async function () {
+  const city = 'Lyon'
+  const lyonCoord = await getGeoCoordinates(city)
+  const lyonMeteo = await getMeteoFromCoordinates(lyonCoord)
+  
+  const symbolIconPath = `weathericons/svg/${lyonMeteo.symbol}.svg`
+
+  const lyonContainer = document.querySelector("#meteo-lyon");
+  lyonContainer.innerHTML = `
+      <h3>
+        <span class="ville">${city}</span> <span class="coord">lat: ${lyonCoord.lat}, lon: ${lyonCoord.lon}</span>
+      </h3>
+      <span class="temperature">${lyonMeteo.temperature}</span>
+      <object class="meteo-icon" type="image/svg+xml" data="${symbolIconPath}"></object>
+  `
+})();
+
+
+
+// Exercice 3
+
+(async function () {
+  const template = document.querySelector("#meteo-card-template");
+  const meteoContainer = document.querySelector("#meteo-dashboard");
+
+  const villesFavorites = ["Paris", "Marseille", "Toulouse", "Villeurbanne", "Lille"];
+  villesFavorites.forEach(async (city) => {
+    const cityCoord = await getGeoCoordinates(city)
+    const cityMeteo = await getMeteoFromCoordinates(cityCoord)
+    const symbolIconPath = `weathericons/svg/${cityMeteo.symbol}.svg`
+
+    const cityCard = template.content.cloneNode(true)
+    cityCard.querySelector(".ville").innerText = city;
+    cityCard.querySelector(".coord").innerText = `lat: ${cityCoord.lat},lon: ${cityCoord.lon}`;
+    cityCard.querySelector(".temperature").innerText = cityMeteo.temperature;
+    cityCard.querySelector(".meteo-icon").data = symbolIconPath;
+
+    meteoContainer.append(cityCard)
+  })
+})();
+
+
+
